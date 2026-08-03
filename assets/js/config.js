4c57c48762844a2acfd05353489d5ccfa45ad881
mczoomzo-design <mczoomzo@gmail.com>
2026-08-03 09:46:52 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,7 +5,7 @@ window.EDUFORUM_CONFIG = {
 
   // วาง URL ของ Web App จาก Google Apps Script ตรงนี้
   // หน้าตาแบบ: https://script.google.com/macros/s/AKfycb.....xxxx/exec
-  API_URL: 'วาง_URL_WEB_APP_ตรงนี้',
+  API_URL: 'https://script.google.com/macros/s/AKfycbx3rNj0z_p2havEfM5YaP8vBvw8CGy27HvPDqsZ-ogBok4hIzzGx6lDmj0PYX2_q7ckSw/exec',
 
   // ชื่อที่แสดงบนหัวเว็บ (ระบบจะดึงจาก Google Sheets ทับให้อีกที)
   SITE_NAME: 'EduForum',
